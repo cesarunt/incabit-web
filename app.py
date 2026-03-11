c3bba69b06c49312e9fb5d509ab2cabc46e2a13b
Cesar Peña <cesarunt@gmail.com>
2026-03-10 23:44:18 -0500

    Actualizando port en main

--- app.py
+++ app.py
@@ -44,5 +44,4 @@ def contacto():
     return render_template("contacto.html", page_title="Contacto")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", "5000")))
