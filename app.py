b2abd1ef7ebb082a6798a837b99732e3988a89b8
Cesar Peña <cesarunt@gmail.com>
2026-04-03 14:55:04 -0500

    two options, runpod or render

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ def procesar_frame_yolo_desde_base64(data_url: str):
         )
 
         result = results[0]
-        annotated = result.plot()
+        # annotated = result.plot()
 
         detecciones = []
         conteo = {}
@@ -296,7 +296,7 @@ def procesar_frame_yolo_desde_base64(data_url: str):
             "ok": True,
             "objects": detecciones,
             "conteo": conteo_formateado,
-            "imagen_procesada": data_url # ASEGÚRATE de que esta línea exista
+            "imagen_procesada": data_url
         }
 
     except Exception as e:
